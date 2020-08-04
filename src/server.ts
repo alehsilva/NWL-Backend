3656d23a2a8a53e972e004368b42062e988cccad
Alexandre <alehsilva@live.com>
2020-08-04 18:45:08 -0300

    Criando pasta database (Knex e Path) e Listando Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,19 @@
 import express from 'express'
 
+const app = express();
+app.use(express.json());
+//GET
+//POST
+//PUT
+//DELETE
+
+//Corpo (Request Body): Dados para criação ou atualização de um registro.
+//Route Params: Identificar qual recurso eu quero atualizar ou deleter.
+//Query Params: Paginação, filtros, ordenação.
+
+app.get('/', (req,res) => {
+    return res.json({massage: 'Hello World'});
+});
+
+//localhost:3333
+app.listen(3333);
