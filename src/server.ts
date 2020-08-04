db5daf5968a4be852c18e23e2f2ecf93b04d36f3
Alexandre <alehsilva@live.com>
2020-08-04 20:29:41 -0300

    Rota de criar aula pronta

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
+app.use(routes);
 //GET
 //POST
 //PUT
@@ -11,9 +14,8 @@ app.use(express.json());
 //Route Params: Identificar qual recurso eu quero atualizar ou deleter.
 //Query Params: Paginação, filtros, ordenação.
 
-app.get('/', (req,res) => {
-    return res.json({massage: 'Hello World'});
-});
+
+
 
 //localhost:3333
 app.listen(3333);
